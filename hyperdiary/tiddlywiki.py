66b02d487114ed4f1d2a4f3e4ee5f6025e4a1011
luphord <luphord@protonmail.com>
2019-06-13 17:48:54 +0200

    refactoring: introduce Tiddler class and use for tiddler output

--- hyperdiary/tiddlywiki.py
+++ hyperdiary/tiddlywiki.py
@@ -2,36 +2,73 @@ import os
 import io
 from . import diary
 
+class Tiddler:
+    def __init__(self, **fields):
+        self.fields = dict(**fields)
+    
+    @property
+    def title(self):
+        return self.fields['title']
+    
+    @property
+    def text(self):
+        return self.fields['text']
+    
+    def __str__(self):
+        return '<Tiddler(title="{0}")>'.format(self.title)
+    
+    def __repr__(self):
+        return 'Tiddler({0})'.format(', '.join(
+            '{}="{}"'.format(k, v) for k, v in self.fields.items()))
+    
+    def _fields_without_text(self):
+        for key, val in self.fields.items():
+            if key.lower() != 'text':
+                yield key, val
+    
+    def to_tid(self):
+        return '\n'.join(['{} = {}'.format(k, v) for k, v in self._fields_without_text()]) \
+            + '\ntype: text/vnd.tiddlywik\n\n' \
+            + self.text
+    
+    @staticmethod
+    def from_entry(dt, entry):
+        tags = []
+        day_text = io.StringIO()
+        for line in entry:
+            day_text.write('* ')
+            for token in diary.tokenize(line):
+                if token.type == diary.TokenType.Id:
+                    day_text.write('[[{}|{}]]'.format(token.text, token.ref))
+                elif token.type == diary.TokenType.Text:
+                    day_text.write(token.text)
+                elif token.type == diary.TokenType.Tag:
+                    tags.append(token.text)
+                else:
+                    raise NotImplementedError('Unknown TokenType')
+            day_text.write('\n')
+        day_text.seek(0)
+        compact_date = '{:04d}{:02d}{:02d}1200000000'.format(dt.year, dt.month, dt.day)
+
+        fields = dict(title=nice_date(dt),
+                      text=day_text.read(),
+                      tags=' '.join(sorted(set(tags))),
+                      created=compact_date,
+                      modified=compact_date)
+        
+        return Tiddler(**fields)
+
+
+
 def nice_date(dt):
     return dt.strftime("%d.%m.%Y")
+
 
 def diary_to_tiddlers(diary_instance, tiddler_dir):
     entries = diary_instance.entries
     os.makedirs(tiddler_dir, exist_ok=True)
 
     for current in sorted(entries.keys()):
+        tiddler = Tiddler.from_entry(current, entries[current])
         with open(os.path.join(tiddler_dir, '{:04d}-{:02d}-{:02d}.tid'.format(current.year, current.month, current.day)), 'w') as f:
-            tags = []
-            day_text = io.StringIO()
-            for line in entries[current]:
-                day_text.write('* ')
-                for token in diary.tokenize(line):
-                    if token.type == diary.TokenType.Id:
-                        day_text.write('[[{}|{}]]'.format(token.text, token.ref))
-                    elif token.type == diary.TokenType.Text:
-                        day_text.write(token.text)
-                    elif token.type == diary.TokenType.Tag:
-                        tags.append(token.text)
-                    else:
-                        raise NotImplementedError('Unknown TokenType')
-                day_text.write('\n')
-            compact_date = '{:04d}{:02d}{:02d}1200000000'.format(current.year, current.month, current.day)
-            f.write('created: {0}\n'.format(compact_date))
-            f.write('modified: {0}\n'.format(compact_date))
-            f.write('tags: {}\n'.format(' '.join(set(tags))))
-            f.write('title: {0}\n'.format(nice_date(current)))
-            f.write('type: text/vnd.tiddlywik\n')
-            f.write('\n')
-            day_text.seek(0)
-            f.write(day_text.read())
-            f.write('\n')
+            f.write(tiddler.to_tid())
--- tests/test_hyperdiary.py
+++ tests/test_hyperdiary.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 import unittest
 from pathlib import Path
 from tempfile import TemporaryDirectory

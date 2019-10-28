4c3535a29039093152a6142811969a82dda471be
luphord <luphord@protonmail.com>
2019-10-28 16:54:53 +0100

    (force) full type annotations for view.py

--- hyperdiary/view.py
+++ hyperdiary/view.py
@@ -1,4 +1,8 @@
-def view(diary, date):
-    print(date)
-    for line in diary.entries[date]:
+from datetime import date
+from .diary import Diary
+
+
+def view(diary: Diary, dt: date) -> None:
+    print(dt)
+    for line in diary.entries[dt]:
         print('- ' + str(line))

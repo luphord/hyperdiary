b8bf972a3bd568dac2bf657882167974f82e60e7
luphord <luphord@protonmail.com>
2019-10-30 07:00:10 +0100

    make linter happy again

--- hyperdiary/check.py
+++ hyperdiary/check.py
@@ -15,10 +15,10 @@ def check(diary: Diary) -> None:
     for dt, _, entry_type in iter_entries(diary.entries):
         if entry_type != EntryType.Line:
             dates_with_bad_types.add(dt)
-    
+
     if dates_with_bad_types:
-        print('WARNING found {} days with type dict or dict-line ' \
-              '(which is deprecated and support will be removed in ' \
+        print('WARNING found {} days with type dict or dict-line '
+              '(which is deprecated and support will be removed in '
               'hyperdiary version 0.5):'.format(len(dates_with_bad_types)))
         for dt in dates_with_bad_types:
             print(dt)

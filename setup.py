e512b5fabb470174c6257b3e037ab3c971cafd77
luphord <luphord@protonmail.com>
2024-05-02 22:15:41 +0200

    Bump version: 0.6.1 → 0.7.0

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/luphord/hyperdiary",
-    version="0.6.1",
+    version="0.7.0",
     zip_safe=False,
 )
